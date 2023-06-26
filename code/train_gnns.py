ca6768b11301628c34e57c614e186e38b318c944
kpputhuveetil <kpputhuveetil@vcu.edu>
2023-06-26 15:21:09 -0400

    cleaned training script, easier to access from cmd args

--- code/train_gnns.py
+++ code/train_gnns.py
@@ -4,40 +4,64 @@ import os
 from bm_dataset import BMDataset
 from gnn_manager import GNN_Manager
 import torch
+import argparse
 
-dataset_dir = '/home/kpputhuveetil/git/vBM-GNNdev/DATASETS/standard'
 
-datasets = [0, 0, 0, 0, 0, 0, 0, 0]
-standard_data = BMDataset(
-        root=dataset_dir, 
-        description='rotate_overhang',
-        voxel_size=0.05, edge_threshold=0.06,
-        rot_draping=True)
-standard_data_3D = BMDataset(
-        root=dataset_dir, 
-        description='3D_representation',
-        voxel_size=0.05, edge_threshold=0.06,
-        rot_draping=False, use_3D=True)
+datasets_dir = './DATASETS'
+
+# may need to uncomment this to run this jupyter cell tag
+parser = argparse.ArgumentParser(description='')
+parser.add_argument('--train-multiple-models', type=bool, default=False)
+parser.add_argument('--dataset-name', type=str) # SET THIS TO THE NAME OF YOUR DATASET FOLDER - this folder should contain another folder called 'raw' with all the pickle files in it
+parser.add_argument('--dataset-desc', type=str)
+parser.add_argument('--rot_draping', type=bool, default=True)
+parser.add_argument('--use_3D', type=bool, default=False)
+parser.add_argument('--model-name', type=str)
+parser.add_argument('--num-train-samp', type=int)
+args = parser.parse_args()
+
+if not args.train_multiple_models:
+
+        dataset_dir = os.path.join(datasets_dir, args.dataset_name)
+        datasets = [BMDataset(
+                root=dataset_dir, 
+                description=args.dataset_desc,
+                voxel_size=0.05, edge_threshold=0.06,
+                rot_draping=args.rot_draping, use_3D=args.use_3D)]
+        
+        model_names = [args.model_name]
+        dataset_sizes = [args.num_train_samp]
+
+else:
+
+        dataset_dir = os.path.join(datasets_dir, 'standard')
+        standard_data = BMDataset(
+                root=dataset_dir, 
+                description='rotate_overhang',
+                voxel_size=0.05, edge_threshold=0.06,
+                rot_draping=True)
+
+        standard_data_3D = BMDataset(
+                root=dataset_dir, 
+                description='3D_representation',
+                voxel_size=0.05, edge_threshold=0.06,
+                rot_draping=False, use_3D=True)
+        
+        model_names = [
+                'standard_2D_0.1k', 
+                'standard_2D_1k',
+                'standard_2D_10k'
+                'standard_3D_10k'
+                ]
+        dataset_sizes = [100, 1000, 10000, 10000]
+        datasets = [standard_data, standard_data, standard_data, standard_data_3D]
+
+
+
 #%%
 # names = ['filt_drape', '2D', 'rot_drape', '3D']
 # names = ['standard_2D_10k', 'standard_2D_50k', 'pose_var_2D_50k', 'blanket_var_2D_50k', 'blanket_var_3D_50k', 'body_var_2D_50k', 'no_human_50k', 'combo_var']
 # dataset_sizes = [10000, 50000, 50000, 50000, 50000, 50000, 50000, 50000]
-
-model_names = [
-        'standard_3D_10k'
-        'body+blanket_var_5k'
-        'combo_var_20k', 
-        'standard_2D_500', 
-        'standard_2D_100',
-        'standard_2D_25k',
-        'pose_var_2D_10k', 
-        'blanket_var_2D_10k', 
-        'blanket_var_3D_10k', 
-        'body_var_2D_10k', 
-        'no_human_10k',
-        'throwaway']
-dataset_sizes = [10000, 500, 100, 25000, 10000, 10000, 10000, 10000, 10000, 1000]
-datasets = [standard_data_3D]
 
 
 for i in range(len(datasets)):
